039dc77152a1c76b4d32720faca1c3937e5d1c39
nikAleksandr <njlyell@gmail.com>
2014-02-26 17:25:02 -0500

    webinar over

--- js/main.js
+++ js/main.js
@@ -271,7 +271,7 @@ function ready(error, us, CountyData) {
 		var fullReport = d3.select("#infoBar").append("div").attr("id", "fullReport");
 			fullReport.append("p").html('<a href="http://www.naco.org/countyTracker">Read the full report</a>');
 			
-		var webinar = fullReport.append("p").html('<a href="https://www2.gotomeeting.com/register/618343202">Register for our free webinar</a>');
+		var webinar = fullReport.append("p").html('<a href="https://www2.gotomeeting.com/register/618343202">Watch the webinar recording</a>');
 		
 		//create the form element for selecting/zooming to states
 			xState = "";
